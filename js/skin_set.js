d0f248e42cb41328e63bbc6f4e6c8840f796a64d
Surplus_Up (2DU) <min08101@naver.com>
2020-04-13 22:03:48 +0900

    acme 다시 다크모드 지원

---
+++ js/skin_set.js
@@ -0,0 +1,66 @@
+function get_post() {
+    check = document.getElementById('invert');
+    if(check.checked === true) {
+        document.cookie = 'invert=1;';
+    } else {
+        document.cookie = 'invert=0;';
+    }
+
+    history.go(0);
+}
+
+function main_load() {
+    var head_data = document.querySelector('head');
+    if(
+        cookies.match(regex_data('invert')) &&
+        cookies.match(regex_data('invert'))[1] === '1'
+    ) {
+        head_data.innerHTML += '<link rel="stylesheet" href="/views/acme/css/set_css/dark.css?ver=1">';
+    }
+}
+
+function regex_data(data) {
+    return new RegExp('(?:^|; )' + data + '=([^;]*)');
+}
+
+var cookies = document.cookie;
+
+function skin_set() {
+    if(window.location.pathname === '/skin_set') {
+        var set_language = {
+            "en-US" : {
+                "save" : "Save",
+                "darkmode" : "Darkmode"
+            }, "ko-KR" : {
+                "save" : "저장",
+                "darkmode" : "다크모드"
+            }
+        }
+
+        var language = cookies.match(regex_data('language'))[1];
+        var user_language = cookies.match(regex_data('user_language'))[1];
+        if(user_language in set_language) {
+            language = user_language;
+        }
+
+        if(!language in set_language) {
+            language = "en-US";
+        }
+
+        var data = document.getElementById("main_skin_set");
+        var set_data = {};
+
+        if(
+            cookies.match(regex_data('invert')) &&
+            cookies.match(regex_data('invert'))[1] === '1'
+        ) {
+            set_data["invert"] = "checked";
+        }
+
+        data.innerHTML = ' \
+            <input ' + set_data["invert"] + ' type="checkbox" id="invert" name="invert" value="invert"> ' + set_language[language]['darkmode'] + ' \
+            <hr class="main_hr"> \
+            <button onclick="get_post();">' + set_language[language]['save'] + '</button> \
+        ';
+    }
+}
